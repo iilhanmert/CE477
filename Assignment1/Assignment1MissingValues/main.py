d935b6ab78fa6bcbd38511dda0830696c875bcf8
Can <99134415+baytekincan@users.noreply.github.com>
2024-04-14 16:10:01 +0300

    main.py-updated

--- Assignment1/Assignment1MissingValues/main.py
+++ Assignment1/Assignment1MissingValues/main.py
@@ -5,6 +5,6 @@ data = pd.read_csv("/Users/canbaytekin/Documents/GitHub/CE477/data/diabetes_pred
 print(data.isna())
 print(data.isnull())
 print(data == float('nan'))
-print(data == str('No Info'))
+
 print(data.isnull().any())
 
